12917b1707c93137cbb7e735199aed84b78fab5b
bartoszbrozek <bartekbrozek@gmail.com>
2021-04-02 19:10:39 +0200

    feat: changed layout of create post page

--- spa-frontend/src/components/Posts/Create.vue
+++ spa-frontend/src/components/Posts/Create.vue
@@ -2,60 +2,77 @@
   <div class="container create-post">
     <div class="content">
       <h1>Create Post</h1>
-
-      <div class="field">
-        <label class="label">Title</label>
-        <div class="control">
-          <input class="input" type="text" v-model="title" />
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label">Link</label>
-        <div class="control">
-          <input class="input" type="text" v-model="link" />
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label">Content</label>
-        <div class="control">
-          <ckeditor
-            :editor="editor"
-            :config="editorConfig"
-            tag-name="textarea"
-            v-model="content"
-          ></ckeditor>
-        </div>
-      </div>
-
-      <h3>Tags</h3>
-      <div class="field has-addons">
-        <div class="control">
-          <input class="input" type="text" placeholder="Use comma as tag separator" v-model="tagTitle" />
-        </div>
-        <div class="control">
-          <a class="button is-success" @click="addTag(tagTitle)"> Add Tag </a>
-        </div>
-      </div>
-
-      <div class="tags-container">
-        <div class="tags has-addons" v-for="(tag, key) in tags" :key="key">
-          <span class="tag is-danger">{{ tag.getTitle() }}</span>
-          <a class="tag is-delete" @click="removeTag(tag)"></a>
-        </div>
-      </div>
-
-      <div class="field">
-        <div class="field-label">
-          <!-- Left empty for spacing -->
-        </div>
-        <div class="field-body">
+      <div class="columns">
+        <div class="column is-three-quarters">
           <div class="field">
+            <label class="label">Title</label>
             <div class="control">
-              <button class="button is-primary" @click="createPost()">
-                Save
-              </button>
+              <input class="input" type="text" v-model="title" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Link</label>
+            <div class="control">
+              <input class="input" type="text" v-model="link" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Content</label>
+            <div class="control">
+              <ckeditor
+                :editor="editor"
+                :config="editorConfig"
+                tag-name="textarea"
+                v-model="content"
+              ></ckeditor>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <label class="label">Content</label>
+          <div class="field has-addons">
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                placeholder="Use comma as tag separator"
+                v-model="tagTitle"
+              />
+            </div>
+            <div class="control">
+              <a class="button is-success" @click="addTag(tagTitle)">
+                Add Tag
+              </a>
+            </div>
+          </div>
+
+          <div class="tags-container">
+            <div class="tags has-addons" v-for="(tag, key) in tags" :key="key">
+              <span class="tag is-danger">{{ tag.getTitle() }}</span>
+              <a class="tag is-delete" @click="removeTag(tag)"></a>
+            </div>
+          </div>
+
+          <div class="field main-buttons-container">
+            <div class="field-label">
+              <!-- Left empty for spacing -->
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <label class="label">&nbsp;</label>
+
+                <div class="control">
+                  <div class="buttons has-addons">
+                    <button class="button">Cancel</button>
+                    <button class="button is-success" @click="createPost()">
+                      Create Post
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
